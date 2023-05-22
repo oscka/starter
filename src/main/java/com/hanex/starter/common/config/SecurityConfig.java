07249c2edba93873cfdd5103556f34e825b73445
jinseul <evelyn.park@osckorea.com>
2023-05-22 11:34:22 +0900

    - h2 console > security config 에 추가

--- src/main/java/com/hanex/starter/common/config/SecurityConfig.java
+++ src/main/java/com/hanex/starter/common/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.hanex.starter.common.config;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -10,6 +11,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter;
 
 @RequiredArgsConstructor
 @EnableWebSecurity
@@ -22,15 +24,21 @@ public class SecurityConfig {
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
-                .csrf().disable()
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .csrf().disable()// csrf 비활성화
+                .formLogin().disable() // 폼 로그인 기능 비활성화
+                .httpBasic().disable() // 베이직 인증 비활성화
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)// 세션 정책 설정 (STATELESS 로 설정하면 쿠키에 세션키를 저장하지 않는다.)
                 .and()
-                .formLogin().disable()
-                .httpBasic().disable()
+                .headers().addHeaderWriter(new XFrameOptionsHeaderWriter(XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN))
+                .and().headers().frameOptions().sameOrigin()
+                .and()
                 .apply(new MyCustomDsl()) // 커스텀 필터 등록
                 .and()
-                .authorizeRequests()
-                .antMatchers("/**").permitAll()
+                .authorizeRequests().antMatchers("/**").permitAll()
+                .and()
+                .authorizeRequests().requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
+                .antMatchers("/").permitAll()
+                .antMatchers("/h2-console/**").permitAll()
                 .and().build();
     }
 
