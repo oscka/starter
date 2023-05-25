6399499017b15895fa101c48e41e2d00767783e6
jinseul <evelyn.park@osckorea.com>
2023-05-25 15:43:41 +0900

    - insert,sql 수정

--- sql/insert.sql
+++ sql/insert.sql
@@ -1,20 +1,23 @@
+
+
 --------------------------------------
 -- auth_mgmt insert
 --------------------------------------
+
 INSERT INTO auth_mgmt
-(auth_id, auth_code, auth_name, sort_seq, created_by, created_at, updated_by, updated_at)
+(id, auth_code, auth_name, sort_seq, created_by, created_at, updated_by, updated_at)
 VALUES('215939fc7d5349738c8ec4a320a1dd02', 'ROLE_0', '시스템관리자', 0, 'SADMIN', NULL, 'SADMIN', NULL);
 INSERT INTO auth_mgmt
-(auth_id, auth_code, auth_name, sort_seq, created_by, created_at, updated_by, updated_at)
+(id, auth_code, auth_name, sort_seq, created_by, created_at, updated_by, updated_at)
 VALUES('a436d3593e3a47f2aa85b521ce1f97a9', 'ROLE_2', '본사직원', 2, 'SADMIN', NULL, 'SADMIN', NULL);
 INSERT INTO auth_mgmt
-(auth_id, auth_code, auth_name, sort_seq, created_by, created_at, updated_by, updated_at)
-VALUES('d2d5cce3d3324df9a26c9a0f8da8572b', 'ROLE_99', '퇴사자', 99, 'SADMIN', NULL, SADMIN, NULL);
+(id, auth_code, auth_name, sort_seq, created_by, created_at, updated_by, updated_at)
+VALUES('d2d5cce3d3324df9a26c9a0f8da8572b', 'ROLE_99', '퇴사자', 99, 'SADMIN', NULL, 'SADMIN', NULL);
 INSERT INTO auth_mgmt
-(auth_id, auth_code, auth_name, sort_seq, created_by, created_at, updated_by, updated_at)
+(id, auth_code, auth_name, sort_seq, created_by, created_at, updated_by, updated_at)
 VALUES('d844ee481095455eadf84981f83394ef', 'ROLE_3', '팀장', 3, 'SADMIN', NULL, 'SADMIN', NULL);
 INSERT INTO auth_mgmt
-(auth_id, auth_code, auth_name, sort_seq, created_by, created_at, updated_by, updated_at)
+(id, auth_code, auth_name, sort_seq, created_by, created_at, updated_by, updated_at)
 VALUES('f0b97eda8e514bd2b01e79b25a8f31dd', 'ROLE_1', '관리자', 1, 'SADMIN', NULL, 'SADMIN', NULL);
 
 
