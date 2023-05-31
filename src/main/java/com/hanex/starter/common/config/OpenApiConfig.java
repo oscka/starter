7143531c218834a11618aa51755140bc32199f55
jinseul <evelyn.park@osckorea.com>
2023-05-31 10:50:59 +0900

    - swagger token 발급 추가,API 그룹화

--- src/main/java/com/hanex/starter/common/config/OpenApiConfig.java
+++ src/main/java/com/hanex/starter/common/config/OpenApiConfig.java
@@ -1,21 +1,20 @@
 package com.hanex.starter.common.config;
 
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springdoc.core.GroupedOpenApi;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 
-@Profile("local")
 @Configuration
 public class OpenApiConfig {
 
     @Bean
-    public GroupedOpenApi publicApi() {
+    public GroupedOpenApi userApi() {
         return GroupedOpenApi.builder()
                 .group("user api")
                 .pathsToMatch("/v1/users/**")
@@ -33,14 +32,29 @@ public class OpenApiConfig {
 
 
     @Bean
-    public OpenAPI openAPI(@Value("${springdoc.version}") String appVersion) {
+    public OpenAPI openAPI() {
+
+        // SecuritySecheme명
+        String jwtSchemeName = "jwtAuth";
+        // API 요청헤더에 인증정보 포함
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList(jwtSchemeName);
+        // SecuritySchemes 등록
+        Components components = new Components()
+                .addSecuritySchemes(jwtSchemeName, new SecurityScheme()
+                        .name(jwtSchemeName)
+                        .type(SecurityScheme.Type.HTTP) // HTTP 방식
+                        .scheme("bearer")
+                        .bearerFormat("JWT")); // 토큰 형식을 지정하는 임의의 문자(Optional)
+
+
         Info info = new Info()
                 .title("Starter API")
-                .version(appVersion)
+                .version("v1.0.0")
                 .description("HanExpress Starter 프로젝트");
 
         return new OpenAPI()
-                .components(new Components())
+                .addSecurityItem(securityRequirement)
+                .components(components)
                 .info(info);
     }
 
